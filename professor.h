83f93b51596a1cc16501937ba375f86c4234f60e
Flavius Gorgonio <flgorgonio@gmail.com>
2021-11-23 19:26:44 -0300

    Separa o Módulo Professor do Módulo Principal

---
+++ professor.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                  Developed by @flgorgonio - 2021.2                      ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+//////
+////// Assinaturas das funções
+//////
+
+void moduloProfessor(void);
+char telaMenuProfessor(void);
+void telaCadastrarProfessor(void);
+void telaPesquisarProfessor(void);
+void telaAlterarProfessor(void);
+void telaExcluirProfessor(void);
